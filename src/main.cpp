6788194feb802a165f1a73bb861cc4276260085d
Maverick <pushkarrajwadhawan@gmail.com>
2026-07-21 18:19:28 +0530

    Code Rook

--- src/main.cpp
+++ src/main.cpp
@@ -6,23 +6,27 @@ using namespace std;
 #include "../include/Piece.h"
 #include "../include/Knight.h"
 #include "../include/Bishop.h"
+#include "../include/Rook.h"
 
 int main() {
     Board board;
 
-    Position center{3, 3};
-    Position p1{5, 5};
-    Position p2{1, 1};
-    auto bishop = make_unique<Bishop>(Color::BLACK);
-    auto knight = make_unique<Knight>(Color::WHITE);
-    auto knight2 = make_unique<Knight>(Color::BLACK);
-    board.placePieceAt(move(bishop), center);
+    Position center = {3, 3};
+    Position p1 = {3, 6};
+    Position p2 = {6, 3};
+
+    auto rook = make_unique<Rook>(Color::BLACK);
+    auto bishop = make_unique<Bishop>(Color::WHITE);
+    auto knight = make_unique<Knight>(Color::BLACK);
+
+    board.placePieceAt(move(rook), center);
     board.placePieceAt(move(knight), p1);
-    board.placePieceAt(move(knight2), p2);
-    vector<Move> possibleMoves = board.getPieceAt(center)->getPseudoLegalMoves(center, board);
-    cout << "Possible Moves: " << possibleMoves.size() << "\n";
-    
-    for(const auto& move: possibleMoves) {
+    board.placePieceAt(move(bishop), p2);
+
+    vector<Move> moves = board.getPieceAt(center)->getPseudoLegalMoves(center, board);
+
+    cout << "Possible Moves: " << (int)moves.size() << "\n";
+    for(auto& move: moves) {
         cout << move.to.row << ", " << move.to.col << (move.isCapture ? " [capture]\n" : "\n");
     }
 }
